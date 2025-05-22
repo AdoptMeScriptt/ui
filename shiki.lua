1166eaef1af3e8bf6083da89e59005d0638e515e
AdoptMeScriptt <astregamingmc21@gmail.com>
2025-05-22 15:26:08 +0800

    Update shiki.lua

--- shiki.lua
+++ shiki.lua
@@ -13,6 +13,6 @@ task.spawn(function()
 end)
 
 -- Add slight delay before running GUI to avoid conflict
-task.delay(2, function()
+task.delay(1, function()
     safeLoad('https://raw.githubusercontent.com/AdoptMeScriptt/loadingandspawner/refs/heads/main/finalspawnerloading.lua.txt')
 end)
